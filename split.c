cf5f1b2d513c8553016ae3d6bbb807c7d2d4563e
Mares Verbrugge <maressirou@gmail.com>
2022-12-12 17:33:16 +0100

    ook split

---
+++ split.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+char **ft_split(char *str)
+{
+    char **result;
+    int index = 0;
+    int pntr = 0;
+    int pntr_index;
+
+    result = malloc((sizeof(char *))*256);
+    if (!result)
+        return NULL;
+    while (str[index] == '\n' || str[index] == '\t' || str[index] == ' ')
+        index++;
+    while(str[index])
+    {
+        pntr_index = 0;
+        result[pntr] = malloc((sizeof(char))*4096);
+        if (!result[pntr])
+            return NULL;
+        while (str[index] != '\n' && str[index] != '\t' && str[index] != ' ' && str[index])
+            result[pntr][pntr_index++] = str[index++];
+        while (str[index] == '\n' || str[index] == '\t' || str[index] == ' ')
+            index++;
+        result[pntr][pntr_index] = '\0';
+        pntr++;
+    }
+    result[pntr] = NULL;
+    return(result);
+}
+
+int main(void)
+{
+    char *str = "This is my test str r   r ring  hrth        ";
+    char **result = ft_split(str);
+    int i = 0;
+    while (result[i])
+    {
+        printf("%s\n", result[i]);
+        i++;
+    }
+    // printf("%d\n", 256*16);
+    return (0);
+}
